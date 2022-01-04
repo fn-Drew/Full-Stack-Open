95009ded2e02e6e352a8e7d9432d0c2885f318e0
MrMisterBean <alutter03@gmail.com>
2022-01-03 22:39:10 -0500

    added phonebook functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,20 +6,43 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson} >
         <div>
-          name: <input />
+          name: <input
+          value={newName}
+          onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
-        <div>debug: {newName}</div>
       </form>
+      <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+          <li>{person.name}</li>
+        )}
+      </ul>
+     ...
     </div>
   )
 }
