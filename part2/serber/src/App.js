3531d71c8421b6e4ffceb1afa4b7871dbb682741
MrMisterBean <alutter03@gmail.com>
2022-01-06 22:01:53 -0500

    app that displays information from local json db

---
+++ part2/serber/src/App.js
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNotes, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  return(
+    <div>
+      {notes.map(note =>
+         <div> {note.content} | {note.date} </div>
+      )}
+    </div>
+  )
+}
+
+export default App
