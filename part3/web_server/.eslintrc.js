b14e0b6d08bc21fc4baf908ada030a8cc1362cd3
MrMisterBean <alutter03@gmail.com>
2023-01-15 16:07:11 -0500

    added and configured ESlint. i like linting

--- part3/web_server/.eslintrc.js
+++ part3/web_server/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     'env': {
-        'browser': true,
+        'node': true,
         'commonjs': true,
         'es2021': true
     },
@@ -26,6 +26,15 @@ module.exports = {
         'semi': [
             'error',
             'never'
-        ]
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0,
     }
 }
