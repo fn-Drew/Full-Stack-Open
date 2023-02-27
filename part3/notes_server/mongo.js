6eaf1156e0a84a6356776eee428c992f836d7d2c
fn-Drew <alutter03@gmail.com>
2023-02-26 20:42:04 -0500

    formatted and green

--- part3/notes_server/mongo.js
+++ part3/notes_server/mongo.js
@@ -1,35 +1,35 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/testing?retryWrites=true&w=majority`
+    `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/testing?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+    content: String,
+    date: Date,
+    important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'CSS is hard',
-  date: new Date(),
-  important: true,
+    content: 'CSS is hard',
+    date: new Date(),
+    important: true,
 })
 
 note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
+    console.log('note saved!')
+    mongoose.connection.close()
 })
 /*
 
