bfc1763d57387d350d96bf04f215246d30f26c47
MrMisterBean <alutter03@gmail.com>
2023-02-26 20:13:39 -0500

    updated notes_server to align with course code

--- part3/notes_server/app.js
+++ part3/notes_server/app.js
@@ -2,23 +2,25 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const usersRouter = require('./controllers/users')
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
--- part3/notes_server/controllers/notes.js
+++ part3/notes_server/controllers/notes.js
@@ -1,69 +1,50 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
-const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
-    response.json(notes)
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.post('/', async (request, response, next) => {
-    const body = request.body
-
-    const user = await User.findById(body.userId)
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        user: user.id,
-        date: new Date()
-    })
-
-    try {
-        const savedNote = await note.save()
-        // save id of note to user who created it
-        user.notes = user.notes.concat(savedNote._id)
-        await user.save()
-
-        response.status(201).json(savedNote)
-    } catch (exception) {
-        next(exception)
-    }
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 module.exports = notesRouter
