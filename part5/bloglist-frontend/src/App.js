141ed3194d129ba5760bf996427a6e54f63af4a0
fn-Drew <alutter03@gmail.com>
2023-03-02 20:20:25 -0500

    refactored LoggedInLayout and related components into new file

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,15 +1,13 @@
 import { useState, useEffect } from 'react'
-import Button from './components/Button'
-import BlogInput from './components/BlogInput'
 import BlogList from './components/BlogList'
 import blogService from './services/blogs'
 import ErrorNotification from './components/ErrorNotification'
 import ConfirmNotification from './components/ConfirmNotification'
 import UserLogin from './components/LoginForm'
+import LoggedInLayout from './components/LoggedInLayout'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
-    const [sorted, setSorted] = useState(true)
     const [errorMessage, setErrorMessage] = useState(null)
     const [confirmMessage, setConfirmMessage] = useState(null)
 
@@ -31,72 +29,18 @@ const App = () => {
         }
     }, [])
 
-    const handleLogout = () => {
-        window.localStorage.removeItem(
-            'loggedBlogAppUser'
-        )
-        setUser(null)
-        setConfirmMessage('You logged out')
-        setTimeout(() => {
-            setConfirmMessage(null)
-        }, 5000)
-
-    }
-    const sortBlogs = () => {
-        setSorted(!sorted)
-        if (sorted) {
-            const sortedBlogs = [...blogs]
-            sortedBlogs.sort((a, b) => b.likes - a.likes)
-            setBlogs(sortedBlogs)
-        } else {
-            // eventually chronological sort?
-            //  for now alphabetical
-            const unsortedBlogs = [...blogs]
-            unsortedBlogs.sort((a, b) => {
-                const titleA = a.title.toUpperCase()
-                const titleB = b.title.toUpperCase()
-                if (titleA < titleB) {
-                    return -1;
-                }
-                if (titleA > titleB) {
-                    return 1;
-                }
-                return 0;
-            });
-            setBlogs(unsortedBlogs)
-        }
-    }
-
-    const LoggedInLayout = () => (
-        <>
-            <p> {user.name} logged in </p>
-            <Button text='logout' mutatingFunction={handleLogout} />
-            <BlogInput
-                user={user}
-                setBlogs={setBlogs}
-                blogs={blogs}
-                setConfirmMessage={setConfirmMessage}
-            />
-            <h2>blogs</h2>
-            <Button
-                text={sorted ?
-                    'alphabetical'
-                    :
-                    'likes'
-                }
-                mutatingFunction={sortBlogs}
-            />
-        </>
-    )
-
     return (
         <div>
             <ErrorNotification message={errorMessage} />
             <ConfirmNotification message={confirmMessage} />
             {user ?
                 <>
-                    <LoggedInLayout />
-                    <BlogList setConfirmMessage={setConfirmMessage} blogs={blogs} setBlogs={setBlogs} user={user} />
+                    <LoggedInLayout user={user} setUser={setUser} blogs={blogs} setBlogs={setBlogs} setConfirmMessage={setConfirmMessage} />
+                    <BlogList
+                        setConfirmMessage={setConfirmMessage}
+                        blogs={blogs}
+                        setBlogs={setBlogs}
+                        user={user} />
                 </>
                 :
                 <>
