1815c23947ea50e9b9903fdb9ca56e5340692d44
fn-Drew <alutter03@gmail.com>
2023-04-01 15:27:29 -0400

    counter app using reducer, context, and custom hooks

---
+++ part6/hook-counter/src/App.js
@@ -0,0 +1,31 @@
+import { useContext, useReducer } from 'react'
+import CounterContext, { useCounterValue, useCounterDispatch } from './CounterContext'
+
+const Display = () => {
+    const counter = useCounterValue()
+    return <div>{counter}</div>
+}
+
+const Button = ({ type, label }) => {
+    const dispatch = useCounterDispatch()
+    return (
+        <button onClick={() => dispatch({ type })}>
+            {label}
+        </button>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <Display />
+            <div>
+                <Button type="INC" label="+" />
+                <Button type="DEC" label="-" />
+                <Button type="ZERO" label="0" />
+            </div>
+        </div>
+    )
+}
+
+export default App
