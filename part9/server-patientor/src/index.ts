0f7ecd811a071110c09c03dfee39a354cefd8b6c
fn-Drew <alutter03@gmail.com>
2023-05-02 14:33:16 -0400

    added api endpoint for getting patient info without ssn

--- part9/server-patientor/src/index.ts
+++ part9/server-patientor/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import patientService from './services/patientService';
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
+
+app.get('/api/patients', (_req, res) => {
+    res.send(patientService.getNonSensitivePatients());
+});
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
